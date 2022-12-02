248b67a3b507c392004d1a5143a9d1f040285525
Jessica Lee <jessica.zy.lee@gmail.com>
2022-12-02 13:53:59 +0000

    Drills 1-4 and password validator challenge complete

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -1,6 +1,11 @@
-# Valid passowords are
+# Valid passwords are
 # - 8 or more characters
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
+    if password.length >= 8 && password.match?(/[!@$%&]/)
+        true
+    else
+        false
+    end
 end
